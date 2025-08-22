2952b64817122f8351dd0d78f896489595d409b8
D.N.Kodithuwakku <140165465+DilshaniNK@users.noreply.github.com>
2025-08-22 09:11:54 +0530

    setup database and create simple CRUD operations in user

---
+++ user-service/src/main/java/com/Code/controller/UserController.java
@@ -0,0 +1,66 @@
+package com.Code.controller;
+
+
+import com.Code.Repository.UserRepository;
+import com.Code.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userRepository.save(user);
+    }
+
+    @GetMapping
+    public List<User> getUsers(){
+        return userRepository.findAll();
+
+    }
+
+    @GetMapping("/{userId}")
+    public User getUserById(@PathVariable("userId") Long id) throws Exception {
+        Optional<User> user = userRepository.findById(id);
+        if(user.isPresent()){
+            return user.get();
+        }
+        throw new Exception("user not found");
+    }
+
+    @PutMapping("/{userId}")
+    public User updateUser(@RequestBody User user,
+                           @PathVariable("userId") Long id) throws Exception {
+        Optional<User> otp = userRepository.findById(id);
+        if(otp.isEmpty()){
+            throw new Exception("User Not Found");
+
+        }
+        User existingUser = otp.get();
+        existingUser.setFullName(user.getFullName());
+        existingUser.setEmail(user.getEmail());
+        existingUser.setRole(user.getRole());
+        return userRepository.save(existingUser);
+
+    }
+
+    @DeleteMapping("/{userId}")
+    public String deleteUser(@PathVariable("userId") Long id) throws Exception {
+        Optional<User> user = userRepository.findById(id);
+        if(user.isEmpty()){
+            throw new Exception("User Not Found");
+        }
+        userRepository.deleteById(id);
+        return "User Deleted Successfully";
+    }
+
+}
---
+++ user-service/src/main/java/com/Code/model/User.java
@@ -0,0 +1,35 @@
+package com.Code.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String fullName;
+
+    private String email;
+
+    private String phone;
+
+    private String role;
+
+    private LocalDate createdAt;
+
+    private LocalDate updatedAt;
+}
